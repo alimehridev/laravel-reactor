51737b18f52360f5f97cfbe143bd17f51caf0747
Ali Mehri <alimehridev@gmail.com>
2021-05-19 15:00:46 +0430

    first commit

---
+++ public/js/resources_js_Site_Test_tsx.js
@@ -0,0 +1,34 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Site_Test_tsx"],{
+
+/***/ "./resources/js/Site/Test.tsx":
+/*!************************************!*\
+  !*** ./resources/js/Site/Test.tsx ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+
+var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+
+var Test = function Test() {
+  var foo = "React";
+  var bar = "TypeScript";
+  return react_1["default"].createElement("h1", null, "Test");
+};
+
+exports.default = Test;
+
+/***/ })
+
+}]);
